02384f1de9d1129d5e24ee786e5d7eab11d36397
mishikabansal <73357090+mishikabansal@users.noreply.github.com>
2022-08-21 01:00:19 +0530

    Add files via upload

---
+++ FITNESS FREAK/report.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Response Page</title>
+<style>
+h1{
+font-size:3em;
+color:maroon;
+}
+</style>
+</head>
+<body>
+<?php
+echo "<h1><center><b>Your response has been submitted!!</b></center></h1>";
+?>
+</body>
+</html>
